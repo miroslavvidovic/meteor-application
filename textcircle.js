2209a9b0b8f86418a0104cb6415830a3c8cc730e
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-04-28 18:45:59 +0200

    Basic html editor

---
+++ textcircle.js
@@ -0,0 +1,47 @@
+this.Documents = new Mongo.Collection("documents");
+
+if (Meteor.isClient) {
+
+  // Update the session current_date variable every 1000 ms
+  Meteor.setInterval(function(){
+    Session.set("current_date", new Date());
+  }, 1000);
+
+  Template.date_display.helpers({ 
+    current_date:function(){
+      return Session.get("current_date");
+    }
+  });
+// find the first document in the Documents colleciton and send back its id
+  Template.editor.helpers({
+    docid:function(){
+      var doc = Documents.findOne();
+      if (doc){
+        return doc._id;
+      }
+      else {
+        return undefined;
+      }
+    },
+    // template helper that configures the CodeMirror editor
+    // you might also want to experiment with the ACE editor
+    config:function(){
+      return function(editor){
+        editor.setOption("mode", "html");
+        editor.on("change", function(cm_editor, info){
+          //console.log(cm_editor.getValue());
+          $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+        });
+      }
+    },
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // startup code that creates a document in case there isn't one yet.
+    if (!Documents.findOne()){// no documents yet!
+        Documents.insert({title:"my new document"});
+    }
+  });
+}
